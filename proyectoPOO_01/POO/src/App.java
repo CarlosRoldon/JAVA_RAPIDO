1ebf8a73132e3959fb33e51b8c039fce185a3009
CarlosRondon <recopilatortwitch@gmail.com>
2024-12-07 12:13:17 -0500

    Integrando Otro proyecto de objetos

---
+++ proyectoPOO_01/POO/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Persona persona1 = new Persona();
+        persona1.nombre = "Andrew Stivenson";
+        persona1.apellido = "Perroso";
+        persona1.edad = 22;
+
+        persona1.ImprimirInformacion();
+    }
+}
---
+++ proyectoPOO_01/POO/src/Persona.java
@@ -0,0 +1,21 @@
+public class Persona {
+    
+    //Atributos Caracteristicas de objeto
+    String nombre;
+    String apellido;
+    int edad;
+    //Direccion direccion;
+
+    
+    //Metodos comportamiento del obejto
+
+    public String nombreCompleto(){
+        return nombre+" "+apellido; 
+    }
+
+    //public String enviaSaludo
+
+    public void ImprimirInformacion(){
+        System.out.println(nombreCompleto()+" con "+edad);
+    }
+}
